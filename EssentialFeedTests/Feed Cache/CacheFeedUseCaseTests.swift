4db57705bccb759140b82bd892da5cee97149da1
Amin Faruq <aminfaruk.fa@gmail.com>
2026-01-30 12:35:20 +0800

    Invert if logic to make code paths easier to follow

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -21,10 +21,14 @@ class LocalFeedLoader {
         store.deleteCachedFeed(completion: { [weak self] error in
             guard let self = self else { return }
             
-            if error == nil {
-                self.store.insert(items, timestamp: self.currentDate(), completion: completion)
+            if let cacheDeletionError = error {
+                completion(cacheDeletionError)
             } else {
-                completion(error)
+                self.store.insert(items, timestamp: self.currentDate()) { [weak self] error in
+                    guard self != nil else { return }
+                    
+                    completion(error)
+                }
             }
         })
     }
