4092f7c0e5a97cdaf0e4a77a472ad9ab0a0ef6c6
Amin Faruq <aminfaruk.fa@gmail.com>
2026-01-31 23:00:58 +0800

    Move `FeedStore` to its own file

---
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,18 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Amin faruq on 31/01/26.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
