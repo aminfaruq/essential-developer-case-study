af1cab55374259a1244f71389cfb2582121910c4
Amin Faruq <aminfaruk.fa@gmail.com>
2026-02-11 17:44:59 +0800

    Add private background context to perform store operations

--- EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FeedStoreSpecs.swift
+++ EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FeedStoreSpecs.swift
@@ -140,7 +140,7 @@ extension FeedStoreSpecs where Self: XCTestCase {
             exp.fulfill()
         }
         
-        wait(for: [exp], timeout: 1.0)
+        wait(for: [exp], timeout: 2.0)
         return deletionError
     }
     
