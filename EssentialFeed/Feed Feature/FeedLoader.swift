c4ab4e535c313b77a8c094aaf068f6e6f4a01a0c
Amin Faruq <aminfaruk.fa@gmail.com>
2026-02-13 22:44:29 +0800

    Nest `LoadFeedResult` into the `FeedLoader` protocol as `FeedLoader.Result` since they're closely related

--- EssentialFeed/Feed Feature/FeedLoader.swift
+++ EssentialFeed/Feed Feature/FeedLoader.swift
@@ -7,14 +7,10 @@
 
 import Foundation
 
-/// Result of a feed loading operation.
-/// - `.success([FeedImage])`: The loader produced a list of domain items
-/// - `.failure(Error)`: The loader failed with a domain-relevant error
 /*public enum LoadFeedResult {
     case success([FeedImage])
     case failure(Error)
 }*/
-public typealias LoadFeedResult = Result<[FeedImage], Error>
 
 /// A use-case abstraction that loads the feed asynchronously.
 ///
@@ -23,6 +19,11 @@ public typealias LoadFeedResult = Result<[FeedImage], Error>
 /// - No threading guarantees; callers should dispatch as needed
 /// - Implementations may source data from network, cache, or composites
 public protocol FeedLoader {
+    /// Result of a feed loading operation.
+    /// - `.success([FeedImage])`: The loader produced a list of domain items
+    /// - `.failure(Error)`: The loader failed with a domain-relevant error
+    typealias Result = Swift.Result<[FeedImage], Error>
+
     /// Starts loading the feed and completes with `LoadFeedResult`.
-    func load(completion: @escaping (LoadFeedResult) -> Void)
+    func load(completion: @escaping (Result) -> Void)
 }
