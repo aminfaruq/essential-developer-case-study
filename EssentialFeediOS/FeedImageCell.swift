2e6006a2e9eb334656d24d8c57db59a12702fd1e
Amin Faruq <aminfaruk.fa@gmail.com>
2026-02-25 15:34:06 +0800

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeed
+//
+//  Created by Amin faruq on 25/02/26.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
