a7702bc14d17f83ea637d7a0b5c5af130b0bccbf
Amin Faruq <aminfaruk.fa@gmail.com>
2026-03-19 08:14:21 +0800

    `RemoteFeedImageDataLoader` does not perform any URL request

---
+++ EssentialFeedTests/Feed API /RemoteImageDataLoaderTests.swift
@@ -0,0 +1,38 @@
+//
+//  RemoteImageDataLoaderTests.swift
+//  EssentialFeed
+//
+//  Created by Amin faruq on 19/03/26.
+//
+
+import XCTest
+import EssentialFeed
+
+class RemoteFeedImageDataLoader {
+    
+    init(client: Any) {
+        
+    }
+    
+}
+
+final class RemoteImageDataLoaderTests: XCTestCase {
+    
+    func test_init_doesNotPerformAnyURLRequest() {
+        let (_, client) = makeSUT()
+        
+        XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+    private func makeSUT(url: URL = anyURL(), file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedImageDataLoader(client: client)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(client, file: file, line: line)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy {
+        var requestedURLs = [URL]()
+    }
+}
