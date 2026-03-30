db4cf93df740e2e217c0ecf4cf71a6a8319626e3
Amin Faruq <aminfaruk.fa@gmail.com>
2026-03-30 22:10:28 +0800

    `LocalFeedLoader` in integration with the `CoreDataFeedStore` does not delete recently saved items saved by separate instances when validating cache, proving we correctly manage the data models on disk.

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -64,19 +64,25 @@ extension LocalFeedLoader: FeedLoader {
 }
 
 extension LocalFeedLoader {
-    public func validateCache() {
+    public typealias ValidationResult = Result<Void, Error>
+    
+    public func validateCache(completion: @escaping (ValidationResult) -> Void = { _ in } ) {
         store.retrieve { [weak self] result in
             guard let self = self else { return }
             
             switch result {
             case .failure:
-                self.store.deleteCachedFeed(completion: { _ in })
+                self.store.deleteCachedFeed(completion: { _ in
+                    completion(.success(()))
+                })
                 
             case let .success(.some(cache)) where !FeedCachePolicy.validate(cache.timestamp, against: self.currentDate()):
-                self.store.deleteCachedFeed(completion: { _ in })
+                self.store.deleteCachedFeed(completion: { _ in
+                    completion(.success(()))
+                })
                 
             case .success:
-                break
+                completion(.success(()))
             }
         }
     }
