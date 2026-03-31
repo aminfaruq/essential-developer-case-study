37d742959ee445e3e800c740310f274c55e8c7f0
Amin Faruq <aminfaruk.fa@gmail.com>
2026-03-31 18:04:14 +0800

    `LocalFeedLoader.validateCache` fails on deletion error of a expired cache

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -75,9 +75,7 @@ extension LocalFeedLoader {
                 self.store.deleteCachedFeed(completion: completion)
                 
             case let .success(.some(cache)) where !FeedCachePolicy.validate(cache.timestamp, against: self.currentDate()):
-                self.store.deleteCachedFeed(completion: { _ in
-                    completion(.success(()))
-                })
+                self.store.deleteCachedFeed(completion: completion)
                 
             case .success:
                 completion(.success(()))
