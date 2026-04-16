4f3f59fe8a94f7a9ad23150788cf15c6ed7ca146
Amin Faruq <aminfaruk.fa@gmail.com>
2026-04-16 21:57:56 +0800

    Remove unnecessary Main storyboard - the initial set up happens in code

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -38,9 +38,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
         
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
         configureWindow()
+        window.makeKeyAndVisible()
     }
     
     func configureWindow() {
@@ -75,4 +78,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         localFeedLoader.validateCache(completion: { _ in })
     }
 }
+
 
