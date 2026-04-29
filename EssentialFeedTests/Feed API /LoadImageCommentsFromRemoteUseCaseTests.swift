6240178a8d849129159f242b72ea00de7bfbb030
Amin Faruq <aminfaruk.fa@gmail.com>
2026-04-29 22:06:20 +0800

    Implement ImageComment mapping

--- EssentialFeedTests/Feed API /LoadImageCommentsFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/Feed API /LoadImageCommentsFromRemoteUseCaseTests.swift
@@ -100,15 +100,15 @@ final class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
         
         let item1 = makeItem(
             id: UUID(),
-            description: nil,
-            location: nil,
-            imageURL: URL(string: "http://a-url.com")!)
+            message: "a message",
+            createdAt: (Date(timeIntervalSince1970: 1598627222), "2020-08-28T15:07:02+00:00"),
+            username: "a username")
         
         let item2 = makeItem(
             id: UUID(),
-            description: "a description",
-            location: "a location",
-            imageURL: URL(string: "http://another-url.com")!)
+            message: "another message",
+            createdAt: (Date(timeIntervalSince1970: 1577881882), "2020-01-01T12:31:22+00:00"),
+            username: "another username")
         
         let items = [item1.model, item2.model]
         
@@ -163,16 +163,18 @@ final class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
     
     /// Creates a `FeedImage` and its equivalent JSON representation.
     /// Nil `description`/`location` values are removed from the JSON using `compactMapValues(_:)`.
-    private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedImage, json: [String: Any]) {
+    private func makeItem(id: UUID, message: String, createdAt: (date: Date, iSO8601String: String), username: String) -> (model: ImageComment, json: [String: Any]) {
         
-        let item = FeedImage(id: id, description: description, location: location, url: imageURL)
+        let item = ImageComment(id: id, message: message, createdAt: createdAt.date, username: username)
         
-        let json = [
+        let json: [String: Any] = [
             "id": id.uuidString,
-            "description": description,
-            "location": location,
-            "image": imageURL.absoluteString
-        ].compactMapValues({ $0 })
+            "message": message,
+            "created_at": createdAt.iSO8601String,
+            "author": [
+                "username" : username
+            ]
+        ]
         
         return (item, json)
     }
