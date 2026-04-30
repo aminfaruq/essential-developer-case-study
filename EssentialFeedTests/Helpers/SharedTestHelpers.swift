65c2179e503aed6c9d014bf671b8c19e99d7b8f3
Amin Faruq <aminfaruk.fa@gmail.com>
2026-04-30 16:06:35 +0800

    Move test helpers to shared scope

--- EssentialFeedTests/Helpers/SharedTestHelpers.swift
+++ EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -13,3 +13,14 @@ func anyURL() -> URL { URL(string: "http://any-url.com")! }
 
 /// Arbitrary data for testing purposes.
 func anyData() -> Data { Data(_: "any data".utf8) }
+
+func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+    let json = ["items": items]
+    return try! JSONSerialization.data(withJSONObject: json)
+}
+
+extension HTTPURLResponse {
+    convenience init(statusCode: Int) {
+        self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+    }
+}
