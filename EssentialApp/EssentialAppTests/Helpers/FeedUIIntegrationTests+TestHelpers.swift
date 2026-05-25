cd01902c8d4dbe3968d5be7771132444a5000536
Amin Faruq <aminfaruk.fa@gmail.com>
2026-05-25 16:20:54 +0800

    Extract helper methods to remove duplication

--- EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+TestHelpers.swift
@@ -67,12 +67,25 @@ extension ListViewController {
     var errorMessage: String? {
         return errorView.message
     }
+    
+    func numberOfRows(in section: Int) -> Int {
+        tableView.numberOfSections > section ? tableView.numberOfRows(inSection: section) : 0
+    }
+    
+    func cell(row: Int, section: Int) -> UITableViewCell? {
+        guard numberOfRows(in: section) > row else {
+            return nil
+        }
+        let ds = tableView.dataSource
+        let index = IndexPath(row: row, section: section)
+        return ds?.tableView(tableView, cellForRowAt: index)
+    }
 }
 
 extension ListViewController {
     
     func numberOfRenderedComments() -> Int {
-        tableView.numberOfSections == 0 ? 0 : tableView.numberOfRows(inSection: commentsSection)
+        numberOfRows(in: commentsSection)
     }
     
     func commentMessage(at row: Int) -> String? {
@@ -88,34 +101,10 @@ extension ListViewController {
     }
     
     private func commentsView(at row: Int) -> ImageCommentCell? {
-        guard numberOfRenderedComments() > row else {
-            return nil
-        }
-        
-        let ds = tableView.dataSource
-        let index = IndexPath(row: row, section: commentsSection)
-        return ds?.tableView(tableView, cellForRowAt: index) as? ImageCommentCell
+        cell(row: row, section: commentsSection) as? ImageCommentCell
     }
     
-    private var commentsSection: Int {
-        return 0
-    }
-}
-
-extension ListViewController {
-    
-    func numberOfRows(in section: Int) -> Int {
-        tableView.numberOfSections > section ? tableView.numberOfRows(inSection: section) : 0
-    }
-    
-    func cell(row: Int, section: Int) -> UITableViewCell? {
-        guard numberOfRows(in: section) > row else {
-            return nil
-        }
-        let ds = tableView.dataSource
-        let index = IndexPath(row: row, section: section)
-        return ds?.tableView(tableView, cellForRowAt: index)
-    }
+    private var commentsSection: Int { 0 }
 }
 
 extension ListViewController {
